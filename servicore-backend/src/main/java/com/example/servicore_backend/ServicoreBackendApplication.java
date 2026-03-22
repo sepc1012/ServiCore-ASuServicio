193d5ed6441c759b6891aadc74db23078babb1e0
sepc1012 <pizarrochauca.sebastian@gmail.com>
2026-03-22 16:32:45 -0500

    commit

---
+++ servicore-backend/src/main/java/com/example/servicore_backend/ServicoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoreBackendApplication.class, args);
+	}
+
+}
